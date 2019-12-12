825df74b7413ab1ec3311a6159ddb0122cdded22
Tanya <tanyaz.228.67@gmail.com>
2019-12-12 13:20:50 +0300

    add main

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -1,6 +1,30 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+            Food[] breakfast = new Food[20];
+            int i = 0;
+
+            for (String arg: args) {
+
+                String[] parts = arg.split("/");
+
+                if (parts[0].equals("Cheese")) {
+                    breakfast[i] = new Cheese();
+                } else
+                if (parts[0].equals("Apple")) {
+                    breakfast[i] = new Apple(parts[1]);
+                }
+                i++;
+            }
+            for (Food item: breakfast)
+                if (item!=null)
+                {
+                    item.consume();
+                }
+                else
+                    { break; }
+
+            System.out.println("Приятного аппетита..");
     }
 }
