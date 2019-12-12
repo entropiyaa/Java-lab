f2f6f38e6fb87e05621c31e88f2e1997e952d45a
Tanya <tanyaz.228.67@gmail.com>
2019-12-12 13:32:46 +0300

    add Parsing command line arguments

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -5,18 +5,70 @@ public class Main {
             Food[] breakfast = new Food[20];
             int i = 0;
 
-            for (String arg: args) {
+        for (String arg: args)
+        {
+            String[] parts = arg.split("/");
 
-                String[] parts = arg.split("/");
-
-                if (parts[0].equals("Cheese")) {
-                    breakfast[i] = new Cheese();
-                } else
-                if (parts[0].equals("Apple")) {
-                    breakfast[i] = new Apple(parts[1]);
-                }
-                i++;
+            if (parts[0].equals("Cheese"))
+            {
+                breakfast[i] = new Cheese();
+            } else
+            if (parts[0].equals("Apple"))
+            {
+                breakfast[i] = new Apple(parts[1]);
+                breakfast[i].addC();
+            } else
+            if(parts[0].equals("Tea"))
+            {
+                breakfast[i] = new Tea(parts[1]);
+            } else
+            if(parts[0].equals("Pie"))
+            {
+                breakfast[i] = new Pie(parts[1]);
+            } else
+            if(parts[0].equals("Milk"))
+            {
+                breakfast[i] = new Milk(parts[1]);
+            } else
+            if(parts[0].equals("Potatoes"))
+            {
+                breakfast[i] = new Potatoes(parts[1]);
+            } else
+            if(parts[0].equals("Burger"))
+            {
+                breakfast[i] = new Burger(parts[1]);
+            } else
+            if(parts[0].equals("Coffee"))
+            {
+                breakfast[i] = new Coffee(parts[1]);
+            } else
+            if(parts[0].equals("IceCream"))
+            {
+                breakfast[i] = new IceCream(parts[1]);
+            } else
+            if(parts[0].equals("ChewingGum"))
+            {
+                breakfast[i] = new ChewingGum(parts[1]);
+            } else
+            if(parts[0].equals("Eggs"))
+            {
+                breakfast[i] = new Eggs(parts[1]);
+            } else
+            if(parts[0].equals("Lemonade"))
+            {
+                breakfast[i] = new Lemonade(parts[1]);
+            } else
+            if(parts[0].equals("Cake"))
+            {
+                breakfast[i] = new Cake(parts[1]);
+            } else
+            if(parts[0].equals("Beef"))
+            {
+                breakfast[i] = new Beef(parts[1]);
             }
+            i++;
+        }
+
             for (Food item: breakfast)
                 if (item!=null)
                 {
